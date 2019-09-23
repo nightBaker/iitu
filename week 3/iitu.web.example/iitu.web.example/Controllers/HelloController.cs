a1de38174b0dd81b9a8199f7d5917b6192354cf4
yerassyl.shalabayev <yerassyl.shalabayev@FHR-DEV-003>
2019-09-23 12:39:35 +0600

    week 3

---
+++ week 3/iitu.web.example/iitu.web.example/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iitu.web.example.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
